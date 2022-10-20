26d20454a8d5316987ec6057b9b12dfb46cf5af8
Tauri <tauri.saar@voco.ee>
2022-10-20 09:20:19 +0300

    MAIN

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const authorForm = document.querySelector("#authorForm");
 const isbnForm = document.querySelector("#isbnForm");
 const masterTable = document.querySelector('#masterTable')
 
+masterTable.addEventListener('click', deleteBook)
 form.addEventListener('submit', addBook)
 
 
@@ -24,4 +25,13 @@ function addBook(){
     cell3.innerHTML = isbnForm.value
     cell4.appendChild(cross)
 }
+
+function deleteBook(event){
+    let selectedBook
+    if (event.target.textContent === "X") {
+        if (confirm("Are you sure you want to remove the book?")) {
+            selectedBook = event.target.parentElement.parentElement.rowIndex
+            masterTable.deleteRow(selectedBook)
+        }
+    }
 }
